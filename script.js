ea457d1c30160bea6e7c2d63d43b4b76ef560fd1
RocketScience <stephanos@malikotsinas.de>
2020-01-15 19:45:09 +0100

    test injection

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-;
+var ext = {
+    flags: {
+        active: false
+    },
+    fn: {},
+    dom: {}
+};
+
+(function() {
+    var container = ext.dom.container = document.createElement('div');
+    container.id = 'test';
+    container.textContent = 'dies ist ein test';
+    container.style.display = 'none';
+    document.body.appendChild(container);
+})();
+
+ext.fn.showInput = function() {
+    ext.dom.container.style.display = 'block';
+};
+
+ext.fn.hideInput = function() {
+    ext.dom.container.style.display = 'none';
+};
